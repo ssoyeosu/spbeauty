f87396fe79bf9e26bf51e56ee70cead8f4d1001d
ssoyeosu <133612524+ssoyeosu@users.noreply.github.com>
2023-05-16 19:46:06 +0000

    Обновил Addresses

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -3,9 +3,9 @@ import SecondaryButton from "../../../../../Components/SecondaryButton";
 import ListElement from "./ListElement";
 function Addresses() {
     return (
-        <div className='d-inline-flex flex-column gap-2 mt-4'>
-            <div className='text-rg'>Адреса заведений</div>
-            <div className='d-inline-flex flex-column gap-3'>
+        <div className='d-inline-flex flex-column gap-4 mt-4 p-4'>
+            <div className='title'>Адреса заведений</div>
+            <div className='d-inline-flex flex-column gap-4'>
                 <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
                 <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
                 <PrimaryButton>Добавить филиал</PrimaryButton>
